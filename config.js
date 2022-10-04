7650cf902e38d4b6e874e8dcb2fb6fc9a6e9cfca
William Georgeson <73065169+WilliamGeo@users.noreply.github.com>
2022-10-04 19:48:41 +0100

    plpl

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'Georgeson', // Required
+  author: 'William Georgeson', // Required
+  description: 'Game Programmer',
+  primaryColor: '#3498db', // Required
+  showHeaderImage: false,
+  showShareButtons: false,
+  postsPerPage: 5, // Required
+  social: {
+    website: 'https://will.georgeson.dev',
+    github: 'https://github.com/WilliamGeo',
+    twitter: 'https://twitter.com/WilliamGoGo_',
+    linkedin: 'https://www.linkedin.com/in/williamgeo1/',
+  },
+  pathPrefix: '/myblog',
+  siteUrl: 'https://georgeson.dev',
+};
